ea7a0ff29b131e424978591562d520e156876545
Baek Han <han.baek.program@gmail.com>
2022-01-10 22:58:37 -0800

    Create project

---
+++ binaryCalculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  binaryCalculator
+//
+//  Created by Hangwonsok Baek on 1/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
