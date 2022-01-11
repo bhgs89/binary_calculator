a7919eda0a6db1be229fdec33a06be1ef63073da
Baek Han <han.baek.program@gmail.com>
2022-01-11 00:44:44 -0800

    Complete calculator -v1

--- binaryCalculator/main.c
+++ binaryCalculator/main.c
@@ -3,9 +3,10 @@
 #include <math.h>
 
 int runBinaryToDecial(void) {
-    int result;
+    int result = 1;
     char binary[73];
     bool status = true;
+    int error_no = 0;
     
     printf("===============================\n");
     printf("***          No space allowed       ***\n");
@@ -14,27 +15,94 @@ int runBinaryToDecial(void) {
     printf("Enter binary: ");
     scanf("%s", binary);
     
-    
     bool positive = true;
+    int pos_idx = 0;
+    int neg_idx = 0;
     int positive_arr[20], negative_arr[52];
     
     for (int i = 0; i < 73; i++) {
         if (binary[i] != '0' && binary[i] != '1' && binary[i] != '.') {
-            status = false;
+            if ((int)binary[i] != 0 && (int)binary[i] != 72) {
+                status = false;
+            }
             break;
         }
         
         if (binary[i] == '.') {
             positive = false;
+            continue;
+        }
+        
+        if (positive && pos_idx > 20) {
+            printf("here1");
+            status = false;
+            error_no = 1;
+            break;
+        } else if (!positive && neg_idx > 52) {
+            printf("here2");
+            status = false;
+            error_no = 2;
+            break;
+        }
+        
+        if (positive) {
+            if (binary[i] == '1') {
+                positive_arr[pos_idx] = 1;
+            } else {
+                positive_arr[pos_idx] = 0;
+            }
+            pos_idx++;
+        } else {
+            if (binary[i] == '1') {
+                negative_arr[neg_idx] = 1;
+            } else {
+                negative_arr[neg_idx] = 0;
+            }
+            neg_idx++;
         }
     }
     
-    if (!status) {
-        printf("You entered invalid binary!!\n");
+    if (status) {
+        int binary_postive_number = 0;
+        if (pos_idx > 0) {
+            for (int i = 0; i < pos_idx; i++) {
+                if (positive_arr[i] > 0) {
+                    binary_postive_number += pow(2, pos_idx - (i + 1));
+                }
+            }
+        }
+                
+        double binary_negative_number = 0;
+        for (int i = 1; i <= neg_idx; i++) {
+            if (negative_arr[i - 1] > 0) {
+                binary_negative_number += pow(2, -i);
+            }
+        }
+
+        double binary_result = 0.0;
+        if (pos_idx == 0 && neg_idx == 0) {
+            printf("Result: none/n");
+        } else {
+            binary_result = binary_postive_number + binary_negative_number;
+            printf("Result: %.52lg\n", binary_result);
+        }
+    } else {
         result = 0;
+        switch (error_no) {
+            case 1:
+                printf("Number of binary number exceed 20!!\n");
+                break;
+                
+            case 2:
+                printf("Number of binary points exceed 52!!\n");
+                break;
+                
+            default:
+                printf("You entered invalid binary!!\n");
+        }
     }
     
-    return 0;
+    return result;
 }
 
 int runDecialToBinary(void) {
